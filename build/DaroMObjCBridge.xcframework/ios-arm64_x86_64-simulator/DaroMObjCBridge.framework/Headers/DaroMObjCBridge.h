7e778a49b82ea5e51bfe02cb125d0312195cf1c4
Finn <fin@delightroom.com>
2025-11-04 22:34:56 +0900

    Bump version to 0.0.1

---
+++ build/DaroMObjCBridge.xcframework/ios-arm64_x86_64-simulator/DaroMObjCBridge.framework/Headers/DaroMObjCBridge.h
@@ -0,0 +1,19 @@
+//
+//  DaroMObjCBridge.h
+//  DaroMObjCBridge
+//
+//  Objective-C Bridge for DaroM SDK
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DaroMObjCBridge.
+FOUNDATION_EXPORT double DaroMObjCBridgeVersionNumber;
+
+//! Project version string for DaroMObjCBridge.
+FOUNDATION_EXPORT const unsigned char DaroMObjCBridgeVersionString[];
+
+// Public headers
+#import <DaroMObjCBridge/DaroObjCInterstitialAdDelegate.h>
+#import <DaroMObjCBridge/DaroObjCRewardedAdDelegate.h>
+#import <DaroMObjCBridge/DaroObjCAppOpenAdDelegate.h>
